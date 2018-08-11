411bdfe21255c2e5dbbee1e1059d908988b14333
Jay Bhagat <jbhagat@bmgdigitaltech.com>
2018-08-11 13:47:55 -0400

    testing circleci

---
+++ PDM-API2-RELOAD/Domain/Entity/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDM.Entity
+{
+    /// <summary>
+    /// Base item class
+    /// </summary>
+    public class Item
+    {
+        public long ID { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TestCircleCIBranches
+# PDM-API2-Reload
+this is the new version of 2 written in .Net Core
